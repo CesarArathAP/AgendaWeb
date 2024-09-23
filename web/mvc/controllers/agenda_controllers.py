26a8f76c7eac16f70df1b1b2d6ab242722bf6336
CesarArathAP <1722110585@utectulancingo.edu.mx>
2024-09-23 00:07:30 -0600

    si

--- web/mvc/controllers/agenda_controllers.py
+++ web/mvc/controllers/agenda_controllers.py
@@ -2,6 +2,8 @@ import web
 
 render = web.template.render('mvc/views', base="templates/layout")
 
+render_index = web.template.render('mvc/views', base="templates/agenda")
+
 class IndexControler:
     def GET(self):
-        return render.index()
+        return render_index.index()
